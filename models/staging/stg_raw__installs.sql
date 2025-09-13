57473ffd7f774637038bf1bce96433cc61449237
NiChr0 <nchr_projects@posteo.com>
2025-09-13 20:17:08 +0200

    build silver layer

--- models/staging/stg_raw__installs.sql
+++ models/staging/stg_raw__installs.sql
@@ -5,8 +5,8 @@ with source as(
 
 renamed as(
 select
-    userId::varchar as user_id,
-    adjustId::varchar as adjust_id,
+    userId::varchar as justplay_user_id,
+    adjustId::varchar as adjust_user_id,
     installedAt::timestamp as installed_at,
     trim(lower(countryCode)) as country_code,
     trim(lower(os_name)) as os_name,
@@ -15,7 +15,7 @@ select
     trim(lower(creative)) as campaign_creative,
     trim(lower(adGroupName)) as ad_group_name,
     trim(lower(trackerName)) as tracker_name,
-    device,
+    trim(lower(device)) as device_name,
     osVersion as os_version,
     limitAdTracking::boolean as has_limit_ad_tracking,
     idfa,
