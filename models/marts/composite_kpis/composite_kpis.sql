d79cac390044857ed95e05e503c8f1a57d6d8dd7
NiChr0 <nchr_projects@posteo.com>
2025-09-14 13:27:09 +0200

    dims and composites

--- models/marts/composite_kpis/composite_kpis.sql
+++ models/marts/composite_kpis/composite_kpis.sql
@@ -1,83 +1,29 @@
+-- models/marts/composite_kpis/composite_kpis_simplified.sql
 {{
   config(
     materialized = 'table'
     )
 }}
 
-with channel_performance as (
-    select *
-    from {{ ref('performance__kpis') }}
-),
-
-user_revenue as (
-    select *
-    from {{ ref('users__kpis') }}
-),
-
-combined_metrics as (
-    select
-        cp.report_date,
-        cp.country_code,
-        cp.os_name,
-        cp.acquisition_channel,
-        cp.campaign_name,
-        cp.campaign_creative,
-        
-        -- Base metrics from channel performance
-        cp.total_ad_spend,
-        cp.total_adjust_installs,
-        cp.total_revenue,
-        cp.ctr,
-        cp.install_conversion_rate,
-        cp.attribution_discrepancy_rate,
-        cp.cost_per_install,
-        cp.revenue_per_install,
-        cp.revenue_per_impression,
-        cp.cpm,
-        
-        -- Base metrics from user revenue
-        ur.total_users_installed,
-        ur.users_with_revenue,
-        ur.arpu,
-        ur.monetization_rate,
-        
-        -- Composite KPIs
-        case 
-            when cp.cost_per_install > 0 and cp.revenue_per_install is not null and ur.monetization_rate is not null
-            then (cp.revenue_per_install * ur.monetization_rate) / cp.cost_per_install 
-            else null 
-        end as marketing_efficiency_score,
-        
-        case 
-            when cp.revenue_per_install is not null and cp.install_conversion_rate is not null and cp.attribution_discrepancy_rate is not null
-            then cp.revenue_per_install * cp.install_conversion_rate * (1 - cp.attribution_discrepancy_rate) 
-            else null 
-        end as channel_quality_index,
-        
-        case 
-            when cp.total_adjust_installs > 0 and ur.arpu is not null
-            then (ur.users_with_revenue::float / cp.total_adjust_installs::float) * ur.arpu 
-            else null 
-        end as user_value_realization_rate,
-        
-        case 
-            when cp.revenue_per_impression is not null and cp.ctr is not null and cp.cpm > 0
-            then (cp.revenue_per_impression * cp.ctr) / (cp.cpm / 1000) 
-            else null 
-        end as media_roi,
-        
-        case 
-            when cp.revenue_per_install is not null and ur.monetization_rate is not null
-            then cp.revenue_per_install * ur.monetization_rate 
-            else null 
-        end as cohort_performance_score
-        
-    from channel_performance cp
-    left join user_revenue ur 
-        on cp.report_date = ur.install_date
-        and cp.country_code = ur.install_country
-        and cp.os_name = ur.install_platform
-        and cp.acquisition_channel = ur.install_channel
+with base_metrics as (
+    select * from {{ ref('performance__kpis') }}
 )
 
-select * from combined_metrics
+select 
+    *,
+    total_revenue / nullif(total_ad_spend, 0) as roas,
+    
+    case 
+        when total_revenue / nullif(total_ad_spend, 0) >= 1.0 then 'highly_profitable'
+        when total_revenue / nullif(total_ad_spend, 0) >= 0.5 then 'profitable'
+        when total_revenue / nullif(total_ad_spend, 0) >= 0.2 then 'barely_profitable'
+        else 'unprofitable'
+    end as channel_profitability_status,
+    
+    case
+        when ctr > 0.02 and attribution_discrepancy_rate < 0.1 then 'high'
+        when ctr > 0.01 and attribution_discrepancy_rate < 0.2 then 'medium'
+        else 'low'
+    end as traffic_quality
+
+from base_metrics
